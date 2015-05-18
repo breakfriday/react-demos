ab82c3e100373deb1af089a69496765848d98f40
breakhappy <break_happy@163.com>
2015-05-18 13:58:34 +0800

    update

---
+++ isomorphic-react-example/app/components/ReactApp.js
@@ -0,0 +1,32 @@
+/** @jsx React.DOM */
+
+var React = require('react/addons');
+
+/* create factory with griddle component */
+var Griddle = React.createFactory(require('griddle-react'));
+
+var fakeData = require('../data/fakeData.js').fakeData;
+var columnMeta = require('../data/columnMeta.js').columnMeta;
+var resultsPerPage = 20;
+var ReactApp = React.createClass({
+
+      componentDidMount: function () {
+        console.log(fakeData);
+          debugger
+      },
+      render: function () {
+        return (
+          <div id="table-area">
+
+             <Griddle results={fakeData}
+                      columnMetadata={columnMeta}
+                      resultsPerPage={resultsPerPage}
+                      tableClassName="table"/>
+
+          </div>
+        )
+      }
+  });
+
+/* Module.exports instead of normal dom mounting */
+module.exports = ReactApp;
